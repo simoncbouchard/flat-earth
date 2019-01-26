eaa6a16fdda569c276624550beb907b943a10c7e
Simon C-Bouchard <simon.cote-bouchard@lg2.com>
2019-01-26 12:08:18 -0500

    Add hero

---
+++ flat-earth/Assets/Scripts/hero.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hero : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+
+    void Start () {
+
+        Debug.Log("test");
+
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+
+    void Update () {
+        
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            rb.velocity = new Vector2(0.4f, 0.0f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow)) {
+            rb.velocity = new Vector2(0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            rb.velocity = new Vector2(-0.4f, 0.0f);
+        }
+
+        if (Input.GetKeyUp(KeyCode.DownArrow)) {
+            rb.velocity = new Vector2(0.0f, 0.0f);
+        }
+    }
+}
