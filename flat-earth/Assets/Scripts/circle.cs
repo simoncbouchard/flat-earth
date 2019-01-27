e1b15fb3d8aa60ee489e0a5372edf3173f3a9c33
Simon C-Bouchard <simon.cote-bouchard@lg2.com>
2019-01-27 12:32:12 -0500

    Add circle machanic

---
+++ flat-earth/Assets/Scripts/circle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circle : MonoBehaviour {
+
+    public float scaleSpeed = 0.05f;
+    Vector3 temp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        temp = transform.localScale;
+
+        temp.x -= scaleSpeed;
+        temp.y -= scaleSpeed;
+
+        if (temp.x > 1) {
+            transform.localScale = temp;
+        }
+        
+    }
+
+/* 
+    void OnTriggerEnter2D(Collider2D collision) {
+        
+        if (collision.gameObject.name == "rob_carbone") {
+            Debug.Log("That's what she said");
+        }
+        
+        
+        //if (collision.gameObject.name == "hero") {
+            //Destroy(this.gameObject);
+        //}
+    }
+    */
+}
--- flat-earth/Assets/Scripts/hero.cs
+++ flat-earth/Assets/Scripts/hero.cs
@@ -9,7 +9,11 @@ public class hero : MonoBehaviour
     public float speed = 10f;
     private Animator anim;
     private bool robMoving;
+
+    private bool robCanMove = true;
     private Vector2 lastMove;
+
+    public GameObject panel;
 
 
     void Start () {
@@ -17,63 +21,84 @@ public class hero : MonoBehaviour
         rb = GetComponent<Rigidbody2D> ();
 
         anim = GetComponent<Animator> ();
+
+        panel.SetActive(false);
     }
 
 
     void Update () {
         
         robMoving = false;
+
+        if (robCanMove) {
         
-        // UpArrow
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            rb.velocity = new Vector2(0.0f, speed);
-            robMoving = true;
-            lastMove = new Vector2 (0f, Input.GetAxisRaw("Vertical"));
+            // UpArrow
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                rb.velocity = new Vector2(0.0f, speed);
+                robMoving = true;
+                lastMove = new Vector2 (0f, Input.GetAxisRaw("Vertical"));
+            }
+
+            if (Input.GetKeyUp(KeyCode.UpArrow)) {
+                rb.velocity = new Vector2(0.0f, 0.0f);
+            }
+
+            // DownArrow
+            if (Input.GetKey(KeyCode.DownArrow)) {
+                rb.velocity = new Vector2(0.0f, -speed);
+                robMoving = true;
+                lastMove = new Vector2 (0f, Input.GetAxisRaw("Vertical"));
+            }
+
+            if (Input.GetKeyUp(KeyCode.DownArrow)) {
+                rb.velocity = new Vector2(0.0f, 0.0f);
+            }
+
+
+            // RightArrow
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                rb.velocity = new Vector2(speed, 0.0f);
+                robMoving = true;
+                lastMove = new Vector2 (Input.GetAxisRaw("Horizontal"), 0f);
+            }
+
+            if (Input.GetKeyUp(KeyCode.RightArrow)) {
+                rb.velocity = new Vector2(0.0f, 0.0f);
+            }
+
+            // LeftArrow
+            if (Input.GetKey(KeyCode.LeftArrow)) {
+                rb.velocity = new Vector2(-speed, 0.0f);
+                robMoving = true;
+                lastMove = new Vector2 (Input.GetAxisRaw("Horizontal"), 0f);
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftArrow)) {
+                rb.velocity = new Vector2(0.0f, 0.0f);
+            }
+
+            anim.SetFloat("moveX", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("moveY", Input.GetAxisRaw("Vertical"));
+            anim.SetBool("rob_move", robMoving);
+            anim.SetFloat("last_moveX", lastMove.x);
+            anim.SetFloat("last_moveY", lastMove.y);
         }
-
-        if (Input.GetKeyUp(KeyCode.UpArrow)) {
-            rb.velocity = new Vector2(0.0f, 0.0f);
-        }
-
-        // DownArrow
-        if (Input.GetKey(KeyCode.DownArrow)) {
-            rb.velocity = new Vector2(0.0f, -speed);
-            robMoving = true;
-            lastMove = new Vector2 (0f, Input.GetAxisRaw("Vertical"));
-        }
-
-        if (Input.GetKeyUp(KeyCode.DownArrow)) {
-            rb.velocity = new Vector2(0.0f, 0.0f);
-        }
-
-
-        // RightArrow
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            rb.velocity = new Vector2(speed, 0.0f);
-            robMoving = true;
-            lastMove = new Vector2 (Input.GetAxisRaw("Horizontal"), 0f);
-        }
-
-        if (Input.GetKeyUp(KeyCode.RightArrow)) {
-            rb.velocity = new Vector2(0.0f, 0.0f);
-        }
-
-        // LeftArrow
-        if (Input.GetKey(KeyCode.LeftArrow)) {
-            rb.velocity = new Vector2(-speed, 0.0f);
-            robMoving = true;
-            lastMove = new Vector2 (Input.GetAxisRaw("Horizontal"), 0f);
-        }
-
-        if (Input.GetKeyUp(KeyCode.LeftArrow)) {
-            rb.velocity = new Vector2(0.0f, 0.0f);
-        }
-
-        anim.SetFloat("moveX", Input.GetAxisRaw("Horizontal"));
-        anim.SetFloat("moveY", Input.GetAxisRaw("Vertical"));
-        anim.SetBool("rob_move", robMoving);
-        anim.SetFloat("last_moveX", lastMove.x);
-        anim.SetFloat("last_moveY", lastMove.y);
 
     }
+
+
+     void OnTriggerExit2D(Collider2D collision) {
+        
+        if (collision.gameObject.name == "Circle") {
+            //Destroy(this.gameObject);
+            //Debug.Log("Dead");
+            robCanMove = false;
+            rb.velocity = new Vector2(0.0f, 0.0f);
+            panel.SetActive(true);
+        }
+        
+        //if (collision.gameObject.name == "hero") {
+        //}
+    }
+    
 }
