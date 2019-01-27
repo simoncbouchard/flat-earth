f8116d72e14b8cdd54c9e9d654b84c95e43b3329
Simon Marcoux <sim_marc@hotmail.com>
2019-01-27 12:59:58 -0500

    Add Doom to America

--- flat-earth/Assets/Scripts/Home.cs
+++ flat-earth/Assets/Scripts/Home.cs
@@ -11,7 +11,6 @@ public class Home : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("TEST TRIGGER");
         //if (Food.GetQuantity)
         Food.emptyAll();
     }
---
+++ flat-earth/Assets/Scripts/WinningGoal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WinningGoal : MonoBehaviour
+{
+    public Text ObjectifDisplay;
+    private int quantity; 
+     
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
